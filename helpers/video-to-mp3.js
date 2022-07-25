8ac0e44a175093e3635f153739a185a672a7252c
Pavel Koshcheev <pavelkoshcheev@yandex.ru>
2022-07-25 23:12:11 +0300

    'fix'

--- helpers/video-to-mp3.js
+++ helpers/video-to-mp3.js
@@ -1,14 +1,6 @@
 const YoutubeMp3Downloader = require('youtube-mp3-downloader');
 const ytdl = require('ytdl-core');
 
-const YD = new YoutubeMp3Downloader({
-    "outputPath": "./",
-    "youtubeVideoQuality": "highestaudio",
-    "queueParallelism": 2,
-    "progressTimeout": 2000,
-    "allowWebm": true
-});
-
 const makeMp3FileFromLink = async(url, successCallback, errorCallback, progressCallback) => {
     if(!ytdl.validateURL(url)) {
         errorCallback('Невалидный URL');
@@ -36,6 +28,14 @@ const makeMp3FileFromLink = async(url, successCallback, errorCallback, progressC
         errorCallback('Невалидный URL');
         return;
     };
+    
+    const YD = new YoutubeMp3Downloader({
+        "outputPath": "./",
+        "youtubeVideoQuality": "highestaudio",
+        "queueParallelism": 2,
+        "progressTimeout": 2000,
+        "allowWebm": true
+    });
     
     YD.download(videoId);
     
--- index.js
+++ index.js
@@ -30,7 +30,7 @@ bot.on('message', async(msg) => {
             });
             return;
         };
-        return await makeMp3FileFromLink(text, successCallback, errorCallback, progressCallback)
+        await makeMp3FileFromLink(text, successCallback, errorCallback, progressCallback)
     } catch(e) {
         return bot.sendMessage(chatId, 'Error')
     }
